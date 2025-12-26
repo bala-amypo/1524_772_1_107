50f1c3f440c0af2e89f4607e25d80a06fac495ee
bala-amypo <bala@amypo.in>
2025-12-26 04:19:31 +0000

    User commit

--- src/main/java/com/example/demo/util/RuleEngineUtil.java
+++ src/main/java/com/example/demo/util/RuleEngineUtil.java
@@ -1,4 +1,17 @@
-public static double computeScore(
-    String description,
-    List<ClaimRule> rules
-)
+public class RuleEngineUtil {
+
+    public static double computeScore(String description, List<ClaimRule> rules) {
+        double score = 0.0;
+        for (ClaimRule rule : rules) {
+            if ("always".equalsIgnoreCase(rule.getConditionExpression())) {
+                score += rule.getWeight();
+            } else if (rule.getConditionExpression().startsWith("description_contains:")) {
+                String key = rule.getConditionExpression().split(":")[1];
+                if (description.toLowerCase().contains(key.toLowerCase())) {
+                    score += rule.getWeight();
+                }
+            }
+        }
+        return Math.min(score, 1.0);
+    }
+}
