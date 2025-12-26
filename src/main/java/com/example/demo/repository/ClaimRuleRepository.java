9284e2fa65829170877342a401c69969a35f9058
bala-amypo <bala@amypo.in>
2025-12-26 05:25:21 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClaimRuleRepository.java
+++ src/main/java/com/example/demo/repository/ClaimRuleRepository.java
@@ -1,9 +1,1 @@
-package com.example.demo.repository;
-import java.util.List;
 
-import com.example.demo.model.ClaimRule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClaimRuleRepository extends JpaRepository<ClaimRule, Long> {
-    List<ClaimRule> findAll();
-}
--- src/main/java/com/example/demo/repository/ParcelRepository.java
+++ src/main/java/com/example/demo/repository/ParcelRepository.java
@@ -1,15 +1,1 @@
-package com.example.demo.repository;
-import org.springframework.web.bind.annotation.*;
-
-import com.example.demo.model.Parcel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ParcelRepository extends JpaRepository<Parcel, Long> {
-
-    Optional<Parcel> findByTrackingNumber(String trackingNumber);
-
-    boolean existsByTrackingNumber(String trackingNumber);
-}
 
