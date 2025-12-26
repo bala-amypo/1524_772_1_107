571fb9f53f3ecd58940c0d5d9a9936715ad714b6
bala-amypo <bala@amypo.in>
2025-12-26 05:37:21 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/DamageClaimRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.DamageClaim;
+
+public interface DamageClaimRepository extends JpaRepository<DamageClaim, Long> {
+    List<DamageClaim> findByParcel_Id(Long parcelId);
+}
--- src/main/java/com/example/demo/repository/ParcelRepository.java
+++ src/main/java/com/example/demo/repository/ParcelRepository.java
@@ -1,1 +1,12 @@
+package com.example.demo.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Parcel;
+
+public interface ParcelRepository extends JpaRepository<Parcel, Long> {
+    boolean existsByTrackingNumber(String trackingNumber);
+    Optional<Parcel> findByTrackingNumber(String trackingNumber);
+}
