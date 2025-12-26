2d27c0b0b073b89d28774f1e46ca8b906ec53a91
bala-amypo <bala@amypo.in>
2025-12-26 05:28:25 +0000

    User commit

--- src/main/java/com/example/demo/servlet/DemoServlet.java
+++
@@ -1,8 +0,0 @@
-@WebServlet(urlPatterns = "/demo")
-public class DemoServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        resp.getWriter().write("OK");
-    }
-}
