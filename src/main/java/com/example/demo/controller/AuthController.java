d2de4e841182fe28c4fbb290c3a754dd13fbea6b
bala-amypo <bala@amypo.in>
2025-12-27 07:49:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,28 +1,29 @@
-package com.example.demo.controller; 
- 
-import io.swagger.v3.oas.annotations.security.SecurityRequirement; 
-import com.example.demo.model.User; 
-import com.example.demo.service.UserService; 
-import org.springframework.web.bind.annotation.*; 
- 
-@RestController 
-@SecurityRequirement(name = "bearerAuth") 
-@RequestMapping("/users") 
-public class AuthController { 
- 
-    private final UserService service; 
- 
-    public AuthController(UserService service) { 
-        this.service = service; 
-    } 
- 
-    @PostMapping("/register") 
-    public User register(@RequestBody User user) { 
-        return service.register(user); 
-    } 
- 
-    @GetMapping("/{email}") 
-    public User getByEmail(@PathVariable String email) { 
-        return service.findByEmail(email); 
-    } 
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        return userService.login(request);
+    }
 }
